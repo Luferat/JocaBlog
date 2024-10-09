964e20f1be9deaae163b0bedb14f38fbc1fe6135
André Luferat <contato@luferat.net>
2024-10-09 08:10:53 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,16 +3,18 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from subprocess import Popen, PIPE, call
 
+appfile = 'app.py'
+
 
 class MyHandler(FileSystemEventHandler):
     def __init__(self, process):
         self.process = process
 
     def on_modified(self, event):
-        if event.src_path.endswith('app.py'):
+        if event.src_path.endswith(appfile):
             print("Arquivo modificado, reiniciando o servidor...")
             self.process.terminate()
-            self.process = Popen(['python', 'app.py'], stdout=PIPE, stderr=PIPE)
+            self.process = Popen(['python', appfile], stdout=PIPE, stderr=PIPE)
             stdout, stderr = self.process.communicate()
             print(stdout.decode())
             print(stderr.decode())
@@ -20,7 +22,7 @@ class MyHandler(FileSystemEventHandler):
 
 if __name__ == '__main__':
     print("Iniciando o servidor...")
-    process = Popen(['python', 'app.py'], stdout=PIPE, stderr=PIPE)
+    process = Popen(['python', appfile], stdout=PIPE, stderr=PIPE)
 
     event_handler = MyHandler(process)
     observer = Observer()
